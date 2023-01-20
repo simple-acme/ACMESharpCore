7aeb3d9fb3ecd21b553f48413f3bda4bc59a207a
Wouter Tinus <win.acme.simple@gmail.com>
2023-01-20 20:48:38 +0100

    cleanup usings

--- src/ACMESharp/Protocol/AcmeProtocolClient.cs
+++ src/ACMESharp/Protocol/AcmeProtocolClient.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Security.Cryptography;
 using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
 using System.Threading.Tasks;
